69c5def76b046156634adf61a00598d6b7c34ba6
Marc PORTET <marc.portet@fiducialinformatique.onmicrosoft.com>
2024-07-25 18:18:26 +0200

    ajout d'un widget google chrome dans une page blazor maui

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -24,6 +25,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, MauiPoCSynapse');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('MauiPoCSynapse app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild, ElementRef, AfterViewInit  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,40 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'MauiPoCSynapse';
+  map: google.maps.Map;
+  @ViewChild('mapWrapper', {static: false}) mapElement: ElementRef;
+
+  public windowWidth: any;
+  public windowHeight: any;
+  
+  ngOnInit() {
+     this.windowWidth = window.innerWidth;
+     this.windowHeight = window.innerHeight;
+  }
+
+  ngAfterViewInit() {
+    this.initializeMap();
+  }
+
+  initializeMap(){
+    const lngLat = new google.maps.LatLng(6.5874964, 3.9886097);
+    const mapOptions: google.maps.MapOptions = {
+      center: lngLat,
+      zoom: 16,
+      fullscreenControl: false,
+      mapTypeControl: false,
+      streetViewControl: false
+    };
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  }
+  
+  @HostListener('window:resize', ['$event'])
+
+  resizeWindow() {
+    this.windowWidth = window.innerWidth;
+    this.windowHeight = window.innerHeight;
+  } 
+  
 }
